2d00c3c9b52d4815e315b3d8c5ebe7d68a580147
Aizha123 <aizha@deltastream.io>
2025-07-30 13:58:29 -0700

    fix unauthorixed problem

--- internal/deltastream/aws/util/kube-client.go
+++ internal/deltastream/aws/util/kube-client.go
@@ -74,11 +74,11 @@ type customPresignClient struct {
 	clusterName string
 }
 
-const cacheTimeout = time.Second * 500 // must be less than X-Amz-Expires
+const cacheTimeout = time.Second * 3500 // must be less than X-Amz-Expires
 
 func (p *customPresignClient) PresignHTTP(ctx context.Context, credentials aws.Credentials, req *http.Request, payloadHash string, service string, region string, signingTime time.Time, optFns ...func(*v4.SignerOptions)) (url string, signedHeader http.Header, err error) {
 	req.Header.Add("x-k8s-aws-id", p.clusterName)
-	req.Header.Add("X-Amz-Expires", "600")
+	req.Header.Add("X-Amz-Expires", "3600")
 	return p.client.PresignHTTP(ctx, credentials, req, payloadHash, service, region, signingTime, optFns...)
 }
 
