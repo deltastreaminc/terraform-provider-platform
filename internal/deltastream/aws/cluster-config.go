48b9601dc76d69761e04b8f7665d9124795b625d
Syed <syed@deltastream.io>
2026-09-10 13:15:41 -0700

    Implement RDS CA certs handling for Aurora
    Add logic to handle RDS CA certificates for Aurora clusters.

--- internal/deltastream/aws/cluster-config.go
+++ internal/deltastream/aws/cluster-config.go
@@ -113,7 +113,11 @@ func updateClusterConfig(ctx context.Context, cfg aws.Config, dp awsconfig.AWSDa
 	cloudImagRegistry := fmt.Sprintf("%s.dkr.ecr.%s.amazonaws.com", config.AccountId.ValueString(), cfg.Region)
 	deployConfigSecret := calcDeploymentConfigSecretName(config, cfg.Region)
 	rdsCACertsRegionalBundleName := fmt.Sprintf("rds-certs-%s-bundle-pem", strings.ToLower(cfg.Region))
-
+	// when using Aurora use multiple region bundle
+	if clusterConfig.RdsControlPlaneUsingAurora.ValueBool() {
+		// identify main geographic location that will host certs for primary dataplane region and aurora geo replicated region, e.g. us for us-east-1
+		rdsCACertsRegionalBundleName = fmt.Sprintf("rds-certs-%s-all-bundle-pem", strings.Split(strings.ToLower(cfg.Region), "-")[0])
+	}
 	hashicorpVaultKeyName := strings.ToLower(fmt.Sprintf("deltastream/%s/ds/%s/aws/%s/vault", config.Stack.ValueString(), config.InfraId.ValueString(), cfg.Region))
 
 	pgWireHostPort := 5432
